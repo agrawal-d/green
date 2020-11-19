eecff475f3ea3c4fd541d0fd616d82006366140a
Divyanshu Agrawal <agrawal.divyanshu@outlook.com>
2020-11-19 17:49:25 +0530

    Basic UI

--- js/details.js
+++ js/details.js
@@ -1,14 +1,51 @@
 const apps = [
-  {
-    name: "discord",
-  },
+  { name: "Jio", icon: "jio.png", high: 1, spy: 1 },
+  { name: "Superset", icon: "superset.png", high: 1 },
+  { name: "Discord", icon: "discord.png", high: 1 },
+  { name: "Soundcore", icon: "soundcore.png", medium: 1 },
+  { name: "Soundcore", icon: "soundcore.png", medium: 1 },
+  { name: "Smart Campus BPHC", icon: "smart-campus.png", medium: 1 },
+  { name: "CMS BPHC", icon: "cms-bphc.png", low: 1 },
+  { name: "Spotify", icon: "spotify.png", low: 1 },
+  { name: "NYTimes", icon: "times.png", low: 1 },
 ];
 
 const el = (name) => document.getElementById(name);
 
-const appList = el("apps-list");
+const appsList = el("apps-list");
+let listString = "";
 
 for (const app of apps) {
-  console.log(app);
-  alert(app.name);
+  listString += `
+  <li class="list-group-item" onclick="t('${app.name}.html')">
+  <div class="row">
+  <div class="col-3 text-center">
+      <img src="img/${app.icon}" alt="${app.name} Icon" class="app">
+  </div>
+  <div class="col-9"><h6>${app.name}</h6>
+  ${
+    app.high
+      ? '<span class="badge badge-pill badge-danger"><span class="icon-warning-sign"></span> High power use</span>'
+      : ""
+  }
+  ${
+    app.spy
+      ? '<span class="badge badge-pill badge-primary">Possible spyware</span>'
+      : ""
+  }
+  ${
+    app.medium
+      ? '<span class="badge badge-pill badge-warning">Medium power use</span>'
+      : ""
+  }
+  ${
+    app.low
+      ? '<span class="badge badge-pill badge-success">Low power use</span>'
+      : ""
+  }
+  </div>
+</div></li>
+  `;
 }
+
+appsList.innerHTML = listString;
--- js/main.js
+++ js/main.js
@@ -2,9 +2,47 @@ window.t = function (href) {
   document.querySelector("body").style.opacity = 0;
   setTimeout(function () {
     window.location.href = href;
-  }, 500);
+  }, 100);
 };
 
 document.addEventListener("DOMContentLoaded", function (event) {
   document.querySelector("body").style.opacity = 1;
 });
+
+function sleep(appname) {
+  let x = confirm(
+    `Are you sure you want to put ${appname} to sleep?\n You may not get notifications.`
+  );
+
+  if (!x) {
+    return;
+  }
+
+  toastr.info(`Putting ${appname} to sleep`);
+  setTimeout(() => {
+    toastr.clear();
+    toastr.success(`${appname} successfully put to sleep.`);
+    $(".more").html(
+      "<center> App has been put to sleep. <br><hr><a class='btn btn-success' onclick='window.location.reload()'>Remove from sleep</a></center>"
+    );
+  }, 2000);
+}
+
+function uninstall(appname) {
+  let x = confirm(
+    `Are you sure you want to completely remove the app ${appname} ?`
+  );
+
+  if (!x) {
+    return;
+  }
+
+  toastr.info(`Uninstalling ${appname}`);
+  setTimeout(() => {
+    toastr.clear();
+
+    toastr.success(`${appname} successfully uninstalled.`);
+
+    $(".more").html("<center> App has been uninstalled. </center>");
+  }, 2000);
+}
